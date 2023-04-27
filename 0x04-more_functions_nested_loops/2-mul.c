980c7908bb3ce98bebfe26ecdda32b0069812196
SalmaElhila <elhilasalma@gmail.com>
2023-04-27 14:42:25 -0700

    from task 0 to task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * mul : multiply two integers
+ * a an b are two variables
+ * Return:multiplication.
+ */
+int mul(int a, int b){
+	return (a * b)
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main(void) - prints numbers fom 0 to  9
+ *
+ * Return: numbers from 0 to 9.
+ */
+void print_numbers(void){
+	char c;
+	for (c <= '0'; c >= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar(\n)
+}
