0d718eeb19e5b919b908dd8d1e4d1bf21520f4b1
SalmaElhila <elhilasalma@gmail.com>
2023-06-12 15:09:22 -0700

    header file is modified

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+
 
 /**
  * _putchar - writes the character c to stdout
