6f4c96ae5c3fade51a0be61c57bd224fde07db59
SalmaElhila <elhilasalma@gmail.com>
2023-06-14 14:58:44 -0700

    header file is updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,17 +6,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
