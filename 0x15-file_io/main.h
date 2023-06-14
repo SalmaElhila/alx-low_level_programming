8dee0c2ba923252e2beed121de27671743367923
SalmaElhila <elhilasalma@gmail.com>
2023-06-14 14:36:47 -0700

    main is modified

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,20 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
-
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+#include <unistd>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
