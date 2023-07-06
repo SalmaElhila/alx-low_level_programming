20a80ee3d791704f60966e49e480f15805f86683
SalmaElhila <elhilasalma@gmail.com>
2023-07-06 05:41:31 -0700

    task1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,17 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int j;
-	unsigned int dec_val = 0;
+	unsigned int result = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
 
 	for (j = 0; b[j]; j++)
 	{
 		if (b[j] < '0' || b[j] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[j] - '0');
+		result = 2 * result + (b[j] - '0');
 	}
 
-	return (dec_val);
+	return (result);
 }
