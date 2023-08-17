97204af59209e2ce0269d25c3c2992b7407a5b5b
SalmaElhila <elhilasalma@gmail.com>
2023-08-17 11:02:50 -0700

    task 7 and 11 updated

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,52 +1,53 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * insert_dnodeint_at_index - A function that inserts a node at
- * position in a list.
- * @h: The double pointer to the head.
- * @idx: The index to insert new node at.
- * @n: The data to add to new node.
- * Return: A pointer to new element, or NULL on failure.
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: address of the new node, or NULL if it fails
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = NULL, *temp = NULL;
-	unsigned int i = 0;
+dlistint_t *new;
+dlistint_t *tmp;
+unsigned int i;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	new_node->n = n;
-	if (!h || !(*h))
-		*h = new_node;
-	else
-	{
-		temp = *h;
-		while (idx != i++ && temp->next)
-			temp = temp->next;
-		if (temp->next)
-			new_node->prev = temp->prev;
-		else
-			new_node->prev = temp;
-		if (idx == i)
-			temp->next = new_node, new_node->prev = temp;
-		else if (idx == i - 1)
-		{
-			if (temp->prev)
-				temp->prev->next = new_node;
-			else
-				*h = new_node;
-			temp->prev = new_node;
-			new_node->next = temp;
-		}
-		else
-		{
-			free(new_node);
-			return (NULL);
-		}
-	}
-	return (new_node);
+if (h == NULL)
+return (NULL);
+tmp = *h;
+i = 0;
+if (idx == 0)
+{
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = *h;
+new->prev = NULL;
+if (*h != NULL)
+(*h)->prev = new;
+*h = new;
+return (new);
+}
+while (tmp != NULL)
+{
+if (i == idx - 1)
+{
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = tmp->next;
+new->prev = tmp;
+tmp->next = new;
+if (new->next != NULL)
+new->next->prev = new;
+return (new);
+}
+tmp = tmp->next;
+i++;
+}
+return (NULL);
 }
